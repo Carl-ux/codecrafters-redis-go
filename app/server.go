7e01b39ce60ec8c8965d4c339e490a36b8d71111
Carl <02carl1126@gmail.com6>
2023-02-22 21:35:42 +0800

    pass 5th stage

--- app/server.go
+++ app/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"net"
@@ -8,7 +10,6 @@ import (
 )
 
 func main() {
-	//stage1 bind to a port
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
@@ -16,34 +17,39 @@ func main() {
 	}
 
 	for {
-		conn, err := l.Accept() //store the connection value
+		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
 
-		go handleConn(conn)
+		go handleConnection(conn)
 	}
 }
 
-func handleConn(conn net.Conn) {
+func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	//stage3 forloop
 	for {
-		buf := make([]byte, 1024) //slice
-		_, err := conn.Read(buf)
-		if err == io.EOF {
+		value, err := DecodeRESP(bufio.NewReader(conn))
+		if errors.Is(err, io.EOF) {
 			break
 		}
 		if err != nil {
-			fmt.Println("error reading from cilent: ", err.Error())
-			os.Exit(1)
+			fmt.Println("Error decoding RESP: ", err.Error())
+			return // Ignore clients that we fail to read from
 		}
-		//stage2 respond to ping cmd
-		if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
-			fmt.Println("PONG Failed")
-			os.Exit(1)
+
+		command := value.Array()[0].String()
+		args := value.Array()[1:]
+
+		switch command {
+		case "ping":
+			conn.Write([]byte("+PONG\r\n"))
+		case "echo":
+			conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(args[0].String()), args[0].String())))
+		default:
+			conn.Write([]byte("-ERR unknown command '" + command + "'\r\n"))
 		}
 	}
 }
