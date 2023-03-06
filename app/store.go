349506466eb6b2772812e80378519c0e78e9b714
Carl <02carl1126@gmail.com6>
2023-03-06 13:18:31 +0800

    pass 7th stage

--- app/store.go
+++ app/store.go
@@ -1,19 +1,49 @@
 package main
 
+import "time"
+
+type ValueWithExpiry struct {
+	value     string
+	expiresAt time.Time
+}
+
 type store struct {
-	data map[string]string
+	data map[string]ValueWithExpiry
+}
+
+func (v ValueWithExpiry) IsExpired() bool {
+	if v.expiresAt.IsZero() {
+		return false
+	}
+	return v.expiresAt.Before(time.Now())
 }
 
 func newStore() *store {
 	return &store{
-		data: make(map[string]string),
+		data: make(map[string]ValueWithExpiry),
 	}
 }
 
-func (kv *store) GET(key string) string {
-	return kv.data[key]
+func (kv *store) GET(key string) (string, bool) {
+	valueWithExpiry, ok := kv.data[key]
+	if !ok {
+		return "", false
+	}
+
+	if valueWithExpiry.IsExpired() {
+		delete(kv.data, key) //delete expired data
+		return "", false
+	}
+	return valueWithExpiry.value, true
 }
 
 func (kv *store) SET(key string, value string) {
-	kv.data[key] = value
+	kv.data[key] = ValueWithExpiry{value: value}
+}
+
+func (kv *store) SetWithExpiry(key string, value string, expiry time.Duration) {
+	kv.data[key] = ValueWithExpiry{
+		value:     value,
+		expiresAt: time.Now().Add(expiry),
+	}
 }
